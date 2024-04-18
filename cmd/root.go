5681246f8ae6404464c166c643388c29c4688b53
tomi-3-0 <calebomoniyitomiwa@gmail.com>
2024-04-18 20:44:53 +0200

    implement the verify function and initialise function

---
+++ cmd/root.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2024 Caleb Omoniyi <calebomoniyitomiwa@gmail.com>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "fiat-shamir",
+	Short: "Fiat-shamir protocol",
+	Long: `Fiat-Shamir heuristic converts interactive "proof of knowledge"
+to non-interactive, letting users prove knowledge without revealing it.
+
+Particularly useful in cryptography.
+This application is a tool to see the protocol in practice.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.fiat-shamir.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ main.go
@@ -0,0 +1,14 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "fiat-shamir/cmd"
+
+func main() {
+	cmd.Execute()
+	cmd.Main()
+}
+
+
